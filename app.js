b3046e618d467088c2f3c67d588f3fb20f954d46
Brandon Lien <b1lien@ucsd.edu>
2019-02-07 14:57:07 -0800

    Lab 5

--- app.js
+++ app.js
@@ -42,3 +42,5 @@ app.get('/', index.view);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+console.log(data)
